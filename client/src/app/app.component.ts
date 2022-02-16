eea1b50344e895ea1b9091954c8287eb83798f20
hhanna94 <heatherhanna1994@gmail.com>
2022-02-16 10:40:29 -0500

    add api/client connection

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5000/api/users')
+    .subscribe(users => {
+      console.log(users);
+    }, error => {
+      console.log(error);
+    })
+  }
 }
