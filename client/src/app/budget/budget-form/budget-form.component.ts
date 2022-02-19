f32520ab7b6745458f1cf89dcfaee888f72c79bf
hhanna94 <heatherhanna1994@gmail.com>
2022-02-19 11:57:58 -0500

    format budget creation form

--- client/src/app/budget/budget-form/budget-form.component.ts
+++ client/src/app/budget/budget-form/budget-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpenseService } from 'src/app/expenses/expense.service';
 
 @Component({
   selector: 'app-budget-form',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-form.component.css']
 })
 export class BudgetFormComponent implements OnInit {
+  categories1 = [];
+  categories2 = [];
+  categories3 = [];
 
-  constructor() { }
+  constructor(private expenseService: ExpenseService) { }
 
   ngOnInit(): void {
+    let categories = this.expenseService.categories; // 11
+    let length = Math.ceil(categories.length/3) // 3
+
+    for (let i = 0; i < categories.length; i++) {
+      if (i < length) { // 3
+        this.categories1.push(categories[i]);
+      } else if (i >= length+1 && i <= (length*2)) {
+        this.categories2.push(categories[i]);
+      } else {
+        this.categories3.push(categories[i]);
+      }
+    }
+    console.log(this.categories1, this.categories2)
   }
 
 }
